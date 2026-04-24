83f36bf599c7ac8c335c0ca1af6a2bed7b26ebc4
ivsantander-dot <iv.santander@duocuc.cl>
2026-04-23 20:45:44 -0400

    inicializado el proyecto

---
+++ src/main/java/com/inovatech/ms_inventario_innovatech/MsInventarioInnovatechApplication.java
@@ -0,0 +1,13 @@
+package com.inovatech.ms_inventario_innovatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInventarioInnovatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInventarioInnovatechApplication.class, args);
+	}
+
+}
